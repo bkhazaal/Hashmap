f559522902dd92ed67a5bbd935b964e101f86cc6
bkhazaal <thajukez101@gmail.com>
2024-03-12 09:33:19 -0700

    yeaaaaa

--- hashmap.js
+++ hashmap.js
@@ -2,8 +2,17 @@ function hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
-    for (let i = 0; i > key.length; i++) {
-        hashCode = primeNumber * hashCode + key.charCode(i);
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
     return hashCode;
+    
 }
+
+const data = {};
+const testkey = process.argv[2];
+
+const hashCode = hash(testkey);
+data[hashCode] = testkey.charAt(0);
+
+console.log(data[hash(testkey)]);
